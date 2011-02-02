524eb414502bbdca04bd09011890e8905ccf653a
Karl Wettin <karl.wettin@kodapan.se>
2011-02-02 01:52:01 +0100

    listerners on primary indices

--- src/main/java/se/kodapan/index/PrimaryIndexListener.java
+++ src/main/java/se/kodapan/index/PrimaryIndexListener.java
@@ -1,8 +1,17 @@
 package se.kodapan.index;
 
+import java.io.Serializable;
+
 /**
+ * todo an object that decouple should trigger listeners in the composite part!
+ *
  * @author kalle
  * @since 2011-02-02 00.36
  */
-public class PrimaryIndexListener {
+public interface PrimaryIndexListener<T extends EntityObject> {
+
+  public abstract void created(T object);
+  public abstract void updated(T object);
+  public abstract void deleted(T object);
+
 }
--- src/main/java/se/kodapan/index/PrimaryIndexListenerAdapter.java
+++ src/main/java/se/kodapan/index/PrimaryIndexListenerAdapter.java
@@ -4,5 +4,18 @@ package se.kodapan.index;
  * @author kalle
  * @since 2011-02-02 00.38
  */
-public class PrimaryIndexListenerAdapter {
+public class PrimaryIndexListenerAdapter implements  PrimaryIndexListener {
+
+  @Override
+  public void created(EntityObject object) {
+  }
+
+  @Override
+  public void updated(EntityObject object) {
+  }
+
+  @Override
+  public void deleted(EntityObject object) {
+  }
+
 }
