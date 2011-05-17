4f1d2a2c996867e99116d9b71eb4675465f853ba
Karl Wettin <karl.wettin@kodapan.se>
2011-05-17 02:19:10 +0200

    well this description was really out of date

---
+++ annotations/src/main/java/se/kodapan/entitystore/Identity.java
@@ -0,0 +1,16 @@
+package se.kodapan.entitystore;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author kalle
+ * @since 2011-04-09 05.48
+ */
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface Identity {
+}
