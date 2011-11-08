ce95124882d6c71a584226682e2273dbb0e68ae4
Karl Wettin <karl.wettin@kodapan.se>
2011-11-08 17:48:27 +0100

    0.2.2

--- extra/src/main/java/se/kodapan/entitystore/AugmentedEntityStore.java
+++ extra/src/main/java/se/kodapan/entitystore/AugmentedEntityStore.java
@@ -3,6 +3,10 @@ package se.kodapan.entitystore;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.reflect.Field;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * @author kalle
  * @since 2011-04-10 11.58
@@ -18,5 +22,28 @@ public class AugmentedEntityStore extends EntityStoreImpl {
   @Override
   public boolean isEntityType(Class entityType) {
     return super.isEntityType(entityType) || entityType.isAnnotationPresent(Entity.class);
+  }
+
+  private Set<Class> registeredPrimaryIndices = new HashSet<Class>();
+
+  @Override
+  public <IdentityType, EntityType> PrimaryIndex<IdentityType, EntityType> getPrimaryIndex(Class<IdentityType> identityType, Class<EntityType> entityType) {
+    PrimaryIndex primaryIndex = super.getPrimaryIndex(identityType, entityType);
+    if (registeredPrimaryIndices.add(entityType)) {
+      for (Field field : entityType.getFields()) {
+        if (field.isAnnotationPresent(Index.class)) {
+          Index key = field.getAnnotation(Index.class);
+          String name = key.name();
+          if (name.isEmpty()) {
+            name = entityType.getSimpleName() + " by " + field.getName();
+          }
+          SecondaryIndex secondaryIndex = new BeanFieldSecondaryIndex(name, primaryIndex, field.getType(), field.getName());
+          if (!registerSecondaryIndex(secondaryIndex)) {
+            throw new RuntimeException("Name for secondary index on " + entityType.getName() + "#" + field.getName() + " is not unique! " + name);
+          }
+        }
+      }
+    }
+    return primaryIndex;
   }
 }
